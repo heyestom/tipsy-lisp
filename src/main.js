a8551fd2e6f000ca3170eae051925bea57d2e07c
Tom Heyes <heyestom@gmail.com>
2019-10-07 13:31:36 +0100

    Remove excessive logging...

--- src/main.js
+++ src/main.js
@@ -2,10 +2,7 @@ const functions = {
     '/': (...args) => args.length === 1 ? 1 / args[0] : args.reduce((total, current) => (total / current)),
     '*': (...args) => args.reduce((total, current) => (total * current)),
     '+': (...args) => args.reduce((total, current) => (total + current)),
-    '-': (...args) => {
-        console.log(args);
-        return args.length === 1 ? 0 - args[0] : args.reduce((total, current) => (total - current));
-    },
+    '-': (...args) => args.length === 1 ? 0 - args[0] : args.reduce((total, current) => (total - current)),
     '>': (...args) => {
         let cond = true;
         args.reduce((x, y) => {
@@ -20,20 +17,14 @@ const functions = {
 
 const specialForms = {
     'if': (AST) => {
-        console.log('IF:');
-        console.log(AST);
         const check = typeof AST.first === 'object' ? parser.eval({
             first: AST.first
         }) : AST.first;
-        console.log('check');
-        console.log(check);
         if (check) {
-            console.log('true case');
             return typeof AST.rest.first === 'object' ?
                 parser.eval(AST.rest.first) :
                 AST.rest.first;
         } else {
-            console.log('false case');
             return typeof AST.rest.rest.first === 'object' ?
                 parser.eval(AST.rest.rest.first) :
                 AST.rest.rest.first;
@@ -89,13 +80,9 @@ const parser = {
         }
     },
     eval(inputAST) {
-        console.log('EVAL');
-        console.log(inputAST);
-
         if (!inputAST) return null;
         // list - make a function call to the first element with the rest  
         if (typeof inputAST.first === 'object' && inputAST.first !== null) {
-            console.log('Found list:');
             let evaluatedList = this.eval(inputAST.first);
             let restEvalueation;
             if (inputAST.rest !== null) {
@@ -117,19 +104,15 @@ const parser = {
 
         if (specialForms[inputAST.first]) {
             const specialForm = specialForms[inputAST.first];
-            console.log('Found special form', inputAST.first);
-            console.log(specialForm);
 
             let specialFormResult = specialForm(inputAST.rest);
 
-            console.log('Returning special form value', specialFormResult);
             return specialFormResult;
         }
 
         // check functions
         if (functions[inputAST.first]) {
             const functionToCall = functions[inputAST.first];
-            console.log('Found function ', inputAST.first);
             const restEvalueation = this.eval(inputAST.rest);
 
             let list;
@@ -138,14 +121,11 @@ const parser = {
             } else {
                 list = [functionToCall, restEvalueation];
             }
-            console.log('returning list :', list);
             return list;
         }
 
         if (inputAST.rest) {
-            console.log('Found value: ', inputAST.first);
             const restEvalueation = this.eval(inputAST.rest);
-            console.log(restEvalueation);
 
             let list;
             if (Array.isArray(restEvalueation)) {
@@ -154,29 +134,16 @@ const parser = {
                 list = [inputAST.first, restEvalueation];
             }
 
-            console.log('returning list', list);
             return list;
         }
 
         // constants
-        console.log('Found constant ', inputAST.first);
         return inputAST.first;
     },
     interprit(tipsyExpression) {
-        console.log(tipsyExpression);
-
         const tokenisedExpresion = this.tokenise(tipsyExpression);
-        console.log('Tokens: ');
-        console.log(tokenisedExpresion);
-
         const AST = this.buildAST(tokenisedExpresion);
-        console.log('AST: ');
-        console.log(AST);
-
         const output = this.eval(AST);
-        console.log('Output: ');
-        console.log(output);
-
         return output;
     }
 };
